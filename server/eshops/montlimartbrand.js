b23a92e79141ef60ae1b1aab13d51a204f449116
tatouti <theojaoudet@live.fr>
2023-02-14 19:09:41 +0100

    Create montlimartbrand.js

---
+++ server/eshops/montlimartbrand.js
@@ -0,0 +1,57 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+const parse = data => {
+  const $ = cheerio.load(data);
+
+  return $('products-list__block products-list__block--col-6--col-12')
+    .map((i, element) => {
+      const name = $(element)
+        .find('.product-miniature__title')
+        .text()
+        .trim();
+      console.log(name);
+      const price = parseInt(
+        $(element)
+          .find('.product-miniature__pricing')
+          .text()
+      );
+      console.log(price);
+      const color = $(element)
+        .find('.product-miniature__color')
+        .text()
+        .trim();
+      console.log(color);
+      return {name,color,price};
+    })
+    .get();
+};
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+module.exports.scrape = async url => {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const body = await response.text();
+
+      return parse(body);
+    }
+
+    console.error(response);
+
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
