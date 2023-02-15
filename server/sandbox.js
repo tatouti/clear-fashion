96fe0c56d676263f06b7dd4c4a268c05c4a34142
tatouti <theojaoudet@live.fr>
2023-02-15 17:50:33 +0100

    Update sandbox.js

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
-const montlimartbrand = require('./eshops/montlimartbrand')
+const montlimartbrand = require('./eshops/montlimartbrand');
+const circlebrand = require('./eshops/circledbrand');
 
 async function sandboxD (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   try {
@@ -22,6 +23,21 @@ async function sandboxM (eshop = 'https://www.montlimart.com/99-vetements') {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
     const products = await montlimartbrand.scrape(eshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+async function sandboxC (eshop = 'https://shop.circlesportswear.com/collections/collection-homme') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+
+    const products = await circlebrand.scrape(eshop);
 
     console.log(products);
     console.log('done');
