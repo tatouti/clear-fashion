c628e3caeb62939a3cef2f04bd0e6d1174df37f3
tatouti <theojaoudet@live.fr>
2023-03-21 16:24:12 +0100

    Update api.js

--- server/api.js
+++ server/api.js
@@ -23,27 +23,7 @@ app.use(helmet());
 
 app.options('*', cors());
 
-/*app.get('/', (request, response) => {
-  const client = get_Client();
-  const MONGODB_DB_NAME = 'ClusterClearFashion';
-  const db =  client.db(MONGODB_DB_NAME);
-  const collection = db.collection(shopName);
-  const result = collection.find(products);
-  response.send(result);
-});*/
-
-app.get('/all', async (request, response) => {
-  try{
-    const client = getClient();
-    const collection = client.db("ClusterClearFashion").collection("GENERAL");
-    response.send({collection});
-  }
-  catch{
-    response.send({error : "Couldn't fetch all"}); 
-  }
-});
-
-app.get('/brands', async (request, response) => {
+app.get('/brand', async (request, response) => {
   try{
     const client = getClient();
     const collection = client.db("ClusterClearFashion").collection("GENERAL");
@@ -60,14 +40,47 @@ app.get('/price', async (request, response) => {
     const client = getClient();
     const collection = client.db("ClusterClearFashion").collection("GENERAL");
     const found = await collection.distinct('price');
-    response.send({result: found});
+    response.send({prices: found});
   }
   catch{
     response.send({error : "Couldn't fetch prices"}); 
   }
 });
 
-app.get('/products/:id', async (request, response) => {
+app.get('/products/search', async (request, response) => {
+  try{
+    const client = getClient();
+    const collection = client.db("ClusterClearFashion").collection("GENERAL");
+
+    var script ={};
+    var limit = request.params.limit;
+    var price = request.params.price;
+    const brand = request.params.brand;
+
+    if(limit == undefined){
+      limit = 12;
+    }
+    else{
+      limit = parseInt(limit);
+    }
+
+    if(brand){
+      script.brand_name = brand;
+    }
+
+    if(price){
+      script.price = {$lte: parseInt(price)};
+    }
+
+    const result = await collection.find(script).limit(limit).toArray();
+    response.send({result : result});
+  }
+  catch{
+    response.send({error : "Couldn't fetch searchs"}); 
+  }
+});
+
+app.get('/products/id', async (request, response) => {
   try{
     const productId = request.params.id;
     const script = {_id: ObjectId(productId)};
@@ -75,7 +88,7 @@ app.get('/products/:id', async (request, response) => {
     const collection = client.db("ClusterClearFashion").collection("GENERAL");
     const found = await collection.find(script).toArray();
     
-    response.send({result: found});
+    response.send({ids: found});
 
   } catch(err) {
 	  response.send({error : "ID not found"});  
