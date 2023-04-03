a29ef776b31b48f1caddd73a7f9e4dd8810aeca6
tatouti <theojaoudet@live.fr>
2023-04-03 15:22:03 +0200

    Update portfolio.js

--- client/v4/portfolio.js
+++ client/v4/portfolio.js
@@ -152,7 +152,7 @@ const renderFavoriteProducts = products => {
     .map(product => {
       return `
       <div class="product" id=${product._id}>
-        <img class="productPhoto" src="${product.photo}">
+        <img src="${product.photo}">
         <span>${product.brand}</span>
         <a href="${product.link}" target="_blank">${product.name}</a>
         <span>${product.price}€</span>
